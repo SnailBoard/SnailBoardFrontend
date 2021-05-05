1cbd830caac800d95bb72dd8e90864427d012b58
rostislavborovyk <45712108+rostislavborovyk@users.noreply.github.com>
2021-05-05 22:41:52 +0300

    Add welcome page (#10) (#11)
    Co-authored-by: n4mespace <41255978+n4mespace@users.noreply.github.com>

--- src/Routing.js
+++ src/Routing.js
@@ -4,6 +4,7 @@ import { Switch } from 'react-router-dom'
 import PublicRoute from './components/PublicRoute'
 import HealthCheck from './components/HealthCheck'
 import LoginPage from './containers/Auth/Login'
+import WelcomePage from './containers/WelcomePage'
 import RegisterPage from './containers/Auth/Register'
 import PrivateRoute from './components/PrivateRoute'
 import HomePage from './containers/HomePage'
@@ -14,6 +15,7 @@ const Routing = () => (
     <PrivateRoute exact path="/health-check" component={HealthCheck} />
     <PrivateRoute exact path="/home" component={HomePage} />
     <PublicRoute exact path="/login" component={LoginPage} />
+    <PublicRoute exact path="/welcome" component={WelcomePage} />
     <PublicRoute exact path="/register" component={RegisterPage} />
     <PublicRoute exact path="/logout" component={LogoutPage} />
   </Switch>
