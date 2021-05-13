0eaaa4936257ec3914368606b6b664a2b5e814fe
rostmoguchiy <rostmoguchiy@gmail.com>
2021-05-13 22:46:58 +0300

    Merge fixes

--- src/containers/HomePage/homeSlice.js
+++ src/containers/HomePage/homeSlice.js
@@ -23,7 +23,6 @@ export const isTeamsFetchingSelector = (state) => state.home.teams.isFetching
 export const isTeamsFailedSelector = (state) => state.home.teams.isFailed
 export const teamsDataSelector = (state) => state.home.teams.data
 
-
 export const homeSlice = createSlice({
   name: 'home',
   initialState,
@@ -62,7 +61,6 @@ export const homeSlice = createSlice({
       state.teams.isFailed = true
       state.teams.isFetching = false
     },
-
   },
 })
 
